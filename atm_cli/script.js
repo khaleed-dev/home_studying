1b951b16ebd77a321a653df2dd8f41b9c0c4a06d
khaleed-dev <69509057+khaleed-dev@users.noreply.github.com>
2022-10-01 00:05:10 +0200

    Add files via upload

---
+++ atm_cli/script.js
@@ -0,0 +1,16 @@
+// CLI Q & A
+/*
+? app idea: simple atm cli app
+
+? app functionality:
+    * What account to access
+    * if account does not exist ask to create account
+    * What to do with that account ?
+        excute command
+           view
+           Deposit
+           withdraw
+? think throug:
+    * 
+
+*/
